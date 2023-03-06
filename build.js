b2d651a3650dbc323523c9b67e1c50acf57ce862
Maxim Schoemaker <maxim.schoemaker@gmail.com>
2023-03-06 16:59:12 +0100

    add newline between build files

--- build.js
+++ build.js
@@ -8,13 +8,11 @@ const source_paths = [
    "src/skulptSetup.js",
 ];
 
-console.log("create or truncate:", target_path);
-fs.writeFileSync(target_path, ""); // create file if it doesn't exist or truncate if it does
-
 console.log("load:", source_paths);
-const files = await Promise.all(source_paths.map(path => fs.promises.readFile(path)));
+const files = await Promise.all(source_paths.map(path => fs.promises.readFile(path, { encoding: "utf-8" })));
 
 console.log("write:", source_paths, "to:", target_path);
-files.forEach(file => fs.writeFileSync(target_path, file, { flag: 'a+' }));
+const concat_files = files.join("\n");
+fs.writeFileSync(target_path, concat_files);
 
 console.log("DONE!")
